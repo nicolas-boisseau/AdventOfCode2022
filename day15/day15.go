d00dc51dfbeb10a212103abcef93269717885126
nicolas.boisseau <nicolas.boisseau@cdiscount.com>
2022-12-14 15:01:27 +0100

    init day 15

---
+++ day15/day15.go
@@ -0,0 +1,11 @@
+package day15
+
+import (
+	"github.com/nicolas-boisseau/AdventOfCode2022/common"
+)
+
+func Process(fileName string, complex bool) int {
+	lines := common.ReadLinesFromFile(fileName)
+
+	return len(lines)
+}
---
+++ day15/day15_test.go
@@ -0,0 +1,33 @@
+package day15
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Process_Sample(t *testing.T) {
+	result := Process("sample.txt", false)
+
+	fmt.Println(result)
+	assert.Equal(t, 24, result)
+}
+
+func Test_Process_Input(t *testing.T) {
+	result := Process("input.txt", false)
+
+	fmt.Println(result)
+}
+
+func Test_Process_Sample_Complex(t *testing.T) {
+	result := Process("sample.txt", true)
+
+	fmt.Println(result)
+	assert.Equal(t, 93, result)
+}
+
+func Test_Process_Input_Complex(t *testing.T) {
+	result := Process("input.txt", true)
+
+	fmt.Println(result)
+}
