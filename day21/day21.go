0658db140e129743d195fd6abb94f538f88b34f9
nicolas.boisseau <nicolas.boisseau@outlook.com>
2022-12-29 22:22:26 +0100

    wip day 21

--- day21/day21.go
+++ day21/day21.go
@@ -1,6 +1,7 @@
 package day21
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/nicolas-boisseau/AdventOfCode2022/common"
 	"strconv"
@@ -8,10 +9,28 @@ import (
 )
 
 type Monkey struct {
-	left, right string
-	name        string
-	op          string
-	yell        func() int
+	left, right   string
+	mLeft, mRight *Monkey
+	name          string
+	op            string
+	yell          func() int64
+	isHumn        bool
+}
+
+func (m *Monkey) String() string {
+	buff := bytes.NewBufferString("")
+	if m.mLeft != nil && m.mRight != nil {
+		fmt.Fprint(buff, "(", m.mLeft.String(), m.op, m.mRight, ")")
+	} else {
+		if !m.isHumn {
+			fmt.Fprintf(buff, "%d", m.yell())
+		} else {
+			fmt.Fprint(buff, "x")
+		}
+
+		//fmt.Fprintf(buff, "[%s]%d", m.name, m.yell())
+	}
+	return buff.String()
 }
 
 type MonkeyList struct {
@@ -59,25 +78,31 @@ func Process(fileName string, complex bool) int {
 			}
 			monkeys.Add(&Monkey{
 				name: splitted[0],
-				yell: func() int {
-					return number
+				yell: func() int64 {
+					return int64(number)
 				},
 			})
 		}
 	}
 
+	// evaluate monkeys
+	for _, m := range monkeys.list {
+		m.mLeft = monkeys.Find(m.left)
+		m.mRight = monkeys.Find(m.right)
+	}
+
 	root := monkeys.Find("root")
 
 	if !complex {
-		return root.yell()
+		return int(root.yell())
 	} else {
 		// change op for root (should return 0 when left and right are equals)
-		root.yell = func() int {
-			return monkeys.Find(root.left).yell() - monkeys.Find(root.right).yell()
+		root.yell = func() int64 {
+			return root.mLeft.yell() - root.mRight.yell()
 		}
 
-		mLeft := monkeys.Find(root.left)
-		mRight := monkeys.Find(root.right)
+		mLeft := root.mLeft
+		mRight := root.mRight
 
 		// Where is "humn" ?!
 		humnSide := mLeft // default to left
@@ -88,125 +113,141 @@ func Process(fileName string, complex bool) int {
 		}
 
 		humn := monkeys.Find("humn")
+		humn.isHumn = true
 
 		i := 0
 		for humnSide != humn {
+			fmt.Println("==", i, "=======================")
+
+			fmt.Println("HUMN SIDE:", humnSide)
+			fmt.Println("OTHER SIDE:", otherSide)
+
+			computedOtherSide := otherSide.yell()
+			fmt.Println("Simplify other side to ", computedOtherSide)
+
+			otherSide.mRight = nil
+			otherSide.mLeft = nil
+			otherSide.right = ""
+			otherSide.left = ""
+			otherSide.yell = func() int64 { return computedOtherSide }
+
 			i++
-			mLeft = monkeys.Find(humnSide.left)
-			mRight = monkeys.Find(humnSide.right)
+			mLeft = humnSide.mLeft
+			mRight = humnSide.mRight
+			humnIsLeft := mLeft.IsInChilds(monkeys, "humn")
+
+			if !humnIsLeft {
+				computed := mLeft.yell()
+
+				mLeft.mRight = nil
+				mLeft.mLeft = nil
+				mLeft.right = ""
+				mLeft.left = ""
+				mLeft.yell = func() int64 { return computed }
+			} else {
+				computed := mRight.yell()
+
+				mRight.mRight = nil
+				mRight.mLeft = nil
+				mRight.right = ""
+				mRight.left = ""
+				mRight.yell = func() int64 { return computed }
+			}
+
 			switch humnSide.op {
+
 			case "+":
-
-				if mRight.IsInChilds(monkeys, "humn") {
-					humnSide = mRight
-					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mLeft.name,
-						op:    "-",
-						yell:  performOp(monkeys, otherSide.name, mLeft.name, "-"),
-					}
-					otherSide = newMonkey
-					monkeys.Add(newMonkey)
-				} else {
-					humnSide = mLeft
-					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mRight.name,
-						op:    "-",
-						yell:  performOp(monkeys, otherSide.name, mRight.name, "-"),
-					}
-					otherSide = newMonkey
-					monkeys.Add(newMonkey)
-				}
+				inverted := "-"
+				humnSide, otherSide = CompensateOtherSide(mRight, monkeys, humnSide, i, otherSide, mLeft, inverted)
 			case "-":
-
-				if mRight.IsInChilds(monkeys, "humn") {
-					humnSide = mRight
-					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mLeft.name,
-						op:    "+",
-						yell:  performOp(monkeys, otherSide.name, mLeft.name, "+"),
-					}
-					otherSide = newMonkey
-					monkeys.Add(newMonkey)
-				} else {
-					humnSide = mLeft
-					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mRight.name,
-						op:    "+",
-						yell:  performOp(monkeys, otherSide.name, mRight.name, "+"),
-					}
-					otherSide = newMonkey
-					monkeys.Add(newMonkey)
-				}
+				inverted := "+"
+				humnSide, otherSide = CompensateOtherSide(mRight, monkeys, humnSide, i, otherSide, mLeft, inverted)
 			case "/":
+				inverted := "*"
+				//humnSide, otherSide = CompensateOtherSide(mRight, monkeys, humnSide, i, otherSide, mLeft, inverted)
 
 				if mRight.IsInChilds(monkeys, "humn") {
-					humnSide = mRight
 					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mLeft.name,
-						op:    "*",
-						yell:  performOp(monkeys, otherSide.name, mLeft.name, "*"),
+						name:   fmt.Sprintf("blabla%d", i),
+						left:   otherSide.name,
+						right:  mRight.name,
+						mLeft:  otherSide,
+						mRight: mRight,
+						op:     inverted,
+						yell:   performOp(monkeys, otherSide.name, mRight.name, inverted),
 					}
-					otherSide = newMonkey
+					humnSide = newMonkey
+					otherSide = mLeft
 					monkeys.Add(newMonkey)
 				} else {
 					humnSide = mLeft
 					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mRight.name,
-						op:    "*",
-						yell:  performOp(monkeys, otherSide.name, mRight.name, "*"),
+						name:   fmt.Sprintf("blabla%d", i),
+						left:   otherSide.name,
+						right:  mRight.name,
+						mLeft:  otherSide,
+						mRight: mRight,
+						op:     inverted,
+						yell:   performOp(monkeys, otherSide.name, mRight.name, inverted),
 					}
 					otherSide = newMonkey
 					monkeys.Add(newMonkey)
 				}
 			case "*":
+				inverted := "/"
+				humnSide, otherSide = CompensateOtherSide(mRight, monkeys, humnSide, i, otherSide, mLeft, inverted)
 
-				if mRight.IsInChilds(monkeys, "humn") {
-					humnSide = mRight
-					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mLeft.name,
-						op:    "/",
-						yell:  performOp(monkeys, otherSide.name, mLeft.name, "/"),
-					}
-					otherSide = newMonkey
-					monkeys.Add(newMonkey)
-				} else {
-					humnSide = mLeft
-					newMonkey := &Monkey{
-						name:  fmt.Sprintf("blabla%d", i),
-						left:  otherSide.name,
-						right: mRight.name,
-						op:    "/",
-						yell:  performOp(monkeys, otherSide.name, mRight.name, "/"),
-					}
-					otherSide = newMonkey
-					monkeys.Add(newMonkey)
-				}
 			}
 		}
 
-		return otherSide.yell()
+		compensatingValue := otherSide.yell()
+
+		humnSide.yell = func() int64 { return compensatingValue }
+		fmt.Println("root.yell() (should be ZERO) = ", root.yell())
+
+		return int(compensatingValue)
 	}
+}
+
+func CompensateOtherSide(mRight *Monkey, monkeys *MonkeyList, humnSide *Monkey, i int, otherSide *Monkey, mLeft *Monkey, inverted string) (*Monkey, *Monkey) {
+	humnIsRight := mRight.IsInChilds(monkeys, "humn")
+	humnIsLeft := mLeft.IsInChilds(monkeys, "humn")
+	fmt.Println("HumnIsLeft:", humnIsLeft, "HumnIsRight:", humnIsRight)
+	if mRight.IsInChilds(monkeys, "humn") {
+		humnSide = mRight
+		newMonkey := &Monkey{
+			name:   fmt.Sprintf("blabla%d", i),
+			left:   otherSide.name,
+			right:  mLeft.name,
+			mLeft:  otherSide,
+			mRight: mLeft,
+			op:     inverted,
+			yell:   performOp(monkeys, otherSide.name, mLeft.name, inverted),
+		}
+		otherSide = newMonkey
+		monkeys.Add(newMonkey)
+	} else {
+		humnSide = mLeft
+		newMonkey := &Monkey{
+			name:   fmt.Sprintf("blabla%d", i),
+			left:   otherSide.name,
+			right:  mRight.name,
+			mLeft:  otherSide,
+			mRight: mRight,
+			op:     inverted,
+			yell:   performOp(monkeys, otherSide.name, mRight.name, inverted),
+		}
+		otherSide = newMonkey
+		monkeys.Add(newMonkey)
+	}
+	return humnSide, otherSide
 }
 
 func (m *Monkey) IsInChilds(monkeys *MonkeyList, name string) bool {
 	if m.name == name {
 		return true
-	} else if m.left != "" && m.right != "" {
-		return monkeys.Find(m.left).IsInChilds(monkeys, name) || monkeys.Find(m.right).IsInChilds(monkeys, name)
+	} else if m.mLeft != nil && m.mRight != nil {
+		return m.mLeft.IsInChilds(monkeys, name) || m.mRight.IsInChilds(monkeys, name)
 	}
 	return false
 }
@@ -226,8 +267,8 @@ func ExtractOpMonkey(splitted []string, monkeys *MonkeyList, op string) *Monkey 
 	}
 }
 
-func performOp(monkeys *MonkeyList, left string, right string, op string) func() int {
-	return func() int {
+func performOp(monkeys *MonkeyList, left string, right string, op string) func() int64 {
+	return func() int64 {
 		mLeft := monkeys.Find(left)
 		mRight := monkeys.Find(right)
 
